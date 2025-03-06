d7e9726382f39b9115cdc4ca882c8e0b532180aa
MatthewM62 <1717212@mukilteo.wednet.edu>
2025-03-06 09:26:54 -0800

    Ranking Every Food 1. Protein 2. Grains 3. Dairy 4. Fruit 5. Vegetables.

---
+++ Assets/Matthew M/Matthew Scripts/PlayerCam.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
+    {
+        
+    }
+}
